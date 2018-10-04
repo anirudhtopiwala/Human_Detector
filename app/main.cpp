87461dcc56640ef06dda1f3824be04884e9dc4ec
anirudhtopiwala <topiwala.anirudh@gmail.com>
2018-10-04 00:35:35 -0400

    changes to sample code

--- app/main.cpp
+++ app/main.cpp
@@ -11,36 +11,14 @@
    * @return 	 return 0
 
    */
-
-static const string keys = "{ help h   |   | print help message }"
-                           "{ camera c | 0 | capture video from camera (device index starting from 0) }"
-                           "{ video v  |   | use video as input }";
-
-int main(int argc, char** argv)
+int main()
 {
-    CommandLineParser parser(argc, argv, keys);
-    parser.about("This sample demonstrates the use ot the HoG descriptor.");
-    if (parser.has("help"))
-    {
-        parser.printMessage();
-        return 0;
-    }
-    int camera = parser.get<int>("camera");
-    string file = parser.get<string>("video");
-    if (!parser.check())
-    {
-        parser.printErrors();
-        return 1;
-    }
 
     VideoCapture cap;
-    if (file.empty())
-        cap.open(camera);
-    else
-        cap.open(file.c_str());
+    cap.open(0);
     if (!cap.isOpened())
     {
-        cout << "Can not open video stream: '" << (file.empty() ? "<camera>" : file) << "'" << endl;
+        cout << "Can not open video stream: " << endl;
         return 2;
     }
 
@@ -61,12 +39,12 @@ int main(int argc, char** argv)
         t = getTickCount() - t;
 
         // show the window
-        {
-            ostringstream buf;
-            buf << "Mode: " << detector.modeName() << " ||| "
-                << "FPS: " << fixed << setprecision(1) << (getTickFrequency() / (double)t);
-            putText(frame, buf.str(), Point(10, 30), FONT_HERSHEY_PLAIN, 2.0, Scalar(0, 0, 255), 2, LINE_AA);
-        }
+//        {
+//            ostringstream buf;
+//            buf << "Mode: " << detector.modeName() << " ||| "
+//                << "FPS: " << fixed << setprecision(1) << (getTickFrequency() / (double)t);
+//            putText(frame, buf.str(), Point(10, 30), FONT_HERSHEY_PLAIN, 2.0, Scalar(0, 0, 255), 2, LINE_AA);
+//        }
         for (vector<Rect>::iterator i = found.begin(); i != found.end(); ++i)
         {
             Rect &r = *i;
