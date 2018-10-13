1e6b381560452c98e9e09e450f858002d22ab213
Ashwin Goyal <ashgo95@terpmail.umd.edu>
2018-10-12 20:33:03 -0400

    Subtask Code : initDet
    Made additional changes in class Detect such as modifying the comments according to cpplint.

--- include/Detect.hpp
+++ include/Detect.hpp
@@ -19,12 +19,7 @@
 
 /*
  * @brief Detector is a class
- *
- * @param1 Grades[5] stores the student grades for 5 students.
- * @param2 ChangeGrade() will change the specific student grade depending upon the input number.
- * @param3 ComputeAverage() compute the average grade of all the students.
  */
-
 class Detect {
     enum Mode { Default, Daimler } m;
     cv::HOGDescriptor hog, hog_d;
@@ -50,7 +45,7 @@ class Detect {
     /*
      * @brief This is the third method of the class. It returns if a human is found in the image.
      *
-     * @ param This method takes an image as input.
+     * @param This method takes an image as input.
      *
      * @return This method returns a vector containing the object Rect which defines the bounding box around the detected humans.
      */
@@ -59,7 +54,7 @@ class Detect {
     /*
      * @brief This is the fourth method of the class. It adjust the bounding box created around a detected human.
      *
-     * @ param This method takes the bounding box detected as input.
+     * @param This method takes the bounding box detected as input.
     */
     void adjustBoundingBox(cv::Rect &);
 
