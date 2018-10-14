0e47bdfdb8595913d69e0fd9a4d81299a67ff624
Ashwin Goyal <ashgo95@terpmail.umd.edu>
2018-10-13 20:02:47 -0400

    Subtask Code: TestDet
    Added a test for the new method added. Also, updated the existing tests. CMakeLists.txt was also updated.

--- test/test.cpp
+++ test/test.cpp
@@ -8,92 +8,119 @@
  */
 
 #include <gtest/gtest.h>
-#include <Detect.hpp>
 #include <Data.hpp>
 #include <Train.hpp>
-
-Data testData1("Data for DataTest 1"), testData2("Data for DataTest 2");
+#include <Detect.hpp>
 
 TEST(DataTest, DataLoadImagesTest) {
-  testData1.loadImages("../data/test");
-  ASSERT_TRUE(testData1.imgList.size() > 0);
+  Data test("Data for DataTest 1");
+  test.loadImages("../data/test");
+  ASSERT_TRUE(test.imgList.size() > 0);
 }
 
 TEST(DataTest, DataSampleImagesTest) {
-  testData2.loadImages("../data/test");
-  std::vector<cv::Mat> orgDataList = testData2.imgList;
-  testData2.sampleImages(cv::Size(100, 100));
-  std::vector<cv::Mat> newDataList = testData2.imgList;
-  int i = 0;
-  bool match = false;
-  for (auto data : orgDataList) {
-    if (data.size() == newDataList[i].size())
+  Data test("Data for DataTest 2");
+  test.loadImages("../data/test");
+  std::vector<cv::Mat> orgDataList = test.imgList;
+  test.sampleImages(cv::Size(100, 100));
+  std::vector<cv::Mat> newDataList = test.imgList;
+  bool match = false, checkSize = true;
+  for (auto i = 0; i < orgDataList.size(); i++) {
+    if (orgDataList[i].size() == newDataList[i].size()) {
       match = true;
-    i++;
+      break;
+    }
+    if (newDataList[i].size() != cv::Size(100,100)) {
+      checkSize = false;
+      break;
+    }
   }
-  ASSERT_TRUE(!match);
+  ASSERT_TRUE(!match & checkSize);
 }
 
-Data testData3("Data for TrainTest 1"), testData4("Data for TrainTest 2"), testData5("Data for TrainTest 3");
-Train trainTest1, trainTest2, trainTest3;
-
 TEST(TrainTest, TrainGetHOGTest) {
-  testData3.loadImages("../data/test");
-  trainTest1.getHOGfeatures(cv::Size(96, 160), testData3.imgList);
-  ASSERT_TRUE(trainTest1.gradientList.size() > 0);
+  Data testData("Data for TrainTest 1");
+  Train test;
+  testData.loadImages("../data/test");
+  test.getHOGfeatures(cv::Size(96, 160), testData.imgList);
+  ASSERT_TRUE(test.gradientList.size() > 0);
 }
 
 TEST(TrainTest, TrainSVMTest) {
-  testData4.loadImages("../data/test");
-  trainTest2.getHOGfeatures(cv::Size(96, 160), testData4.imgList);
-  size_t posCount = trainTest2.gradientList.size();
-  trainTest2.labels.assign(posCount/2, 1);
-  trainTest2.labels.insert(trainTest2.labels.end(), posCount - trainTest2.labels.size(), -1);
-  ASSERT_TRUE(trainTest2.classifier->getSupportVectors().empty());
-  trainTest2.trainSVM();
-  ASSERT_TRUE(!trainTest2.classifier->getSupportVectors().empty());
+  Data testData("Data for TrainTest 2");
+  Train test;
+  testData.loadImages("../data/test");
+  test.getHOGfeatures(cv::Size(96, 160), testData.imgList);
+  test.labels.assign(test.gradientList.size()/2, 1);
+  test.labels.insert(test.labels.end(), test.gradientList.size() - test.labels.size(), -1);
+  bool cond = test.classifier->getSupportVectors().empty();
+  test.trainSVM();
+  cond = cond & !test.classifier->getSupportVectors().empty();
+  ASSERT_TRUE(cond);
 }
 
 TEST(TrainTest, TrainGetClassifierTest) {
-  testData5.loadImages("../data/test");
-  trainTest3.getHOGfeatures(cv::Size(96, 160), testData5.imgList);
-  size_t posCount = trainTest3.gradientList.size();
-  trainTest3.labels.assign(posCount/2, 1);
-  trainTest3.labels.insert(trainTest3.labels.end(), posCount - trainTest3.labels.size(), -1);
-  trainTest3.trainSVM();
-  ASSERT_TRUE(trainTest3.getClassifier().size() != 0);
+  Data testData("Data for TrainTest 3");
+  Train test;
+  testData.loadImages("../data/test");
+  test.getHOGfeatures(cv::Size(96, 160), testData.imgList);
+  test.labels.assign(test.gradientList.size()/2, 1);
+  test.labels.insert(test.labels.end(), test.gradientList.size() - test.labels.size(), -1);
+  test.trainSVM();
+  ASSERT_TRUE(test.getClassifier().size() != 0);
 }
 
-cv::HOGDescriptor hog;
-hog.setSVMDetector(cv::HOGDescriptor::getDefaultPeopleDetector());
-Detect test1(hog), test2(hog), test3(hog), test4(hog);
 TEST(DetectTest, DetectModeNameTest) {
-  ASSERT_EQ("Default", test1.modeName());
+  cv::HOGDescriptor hog;
+  hog.setSVMDetector(cv::HOGDescriptor::getDefaultPeopleDetector());
+  Detect test(hog);
+  ASSERT_EQ("Default", test.modeName());
 }
 
 TEST(DetectTest, DetectToggleTest) {
-  ASSERT_EQ("Default", test2.modeName());
-  test2.toggleMode();
-  ASSERT_EQ("User", test2.modeName());
+  cv::HOGDescriptor hog;
+  hog.setSVMDetector(cv::HOGDescriptor::getDefaultPeopleDetector());
+  Detect test(hog);
+  test.toggleMode();
+  ASSERT_EQ("User", test.modeName());
 }
 
 TEST(DetectTest, DetectHumansTest) {
+  cv::HOGDescriptor hog;
+  hog.setSVMDetector(cv::HOGDescriptor::getDefaultPeopleDetector());
+  Detect test(hog);
   cv::Rect orgBox(115,6,238,475);  //[238 x 475 from (115, 6)]
   std::string imageName("../data/test/pedestrian_5.jpg");
   cv::Mat img = cv::imread(imageName, CV_LOAD_IMAGE_COLOR);
-  std::vector<cv::Rect> found = test3.findHumans(img);
+  std::vector<cv::Rect> found = test.findHumans(img);
   std::vector<cv::Rect>::iterator i = found.begin();
   cv::Rect &r = *i;
   ASSERT_TRUE((orgBox.area() - (orgBox & r).area()) < 500);
 }
 
 TEST(DetectTest, DetectAdjustBoxTest) {
+  cv::HOGDescriptor hog;
+  hog.setSVMDetector(cv::HOGDescriptor::getDefaultPeopleDetector());
+  Detect test(hog);
   cv::Rect newBox(139,39,190,380);  //[190 x 380 from (139, 39)]
   std::string imageName("../data/test/pedestrian_5.jpg");
   cv::Mat img = cv::imread(imageName, CV_LOAD_IMAGE_COLOR);
-  std::vector<cv::Rect> found = test4.findHumans(img);
+  std::vector<cv::Rect> found = test.findHumans(img);
   std::vector<cv::Rect>::iterator i = found.begin();
   cv::Rect &r = *i;
-  test2.adjustBoundingBox(r);
+  test.adjustBoundingBox(r);
   ASSERT_TRUE((newBox.area() - (newBox & r).area()) < 500);
+}
+
+TEST(DetectTest, DetectTestClassifierTest) {
+  cv::HOGDescriptor hog;
+  hog.setSVMDetector(cv::HOGDescriptor::getDefaultPeopleDetector());
+  Detect test1(hog), test2(hog);
+  std::string imageName("../data/test/pedestrian_5.jpg");
+  cv::Mat img = cv::imread(imageName, CV_LOAD_IMAGE_COLOR);
+  std::vector<cv::Rect> found = test1.findHumans(img);
+  std::vector<cv::Rect>::iterator i = found.begin();
+  cv::Rect &orgBox = *i;
+  cv::Rect r = test2.testClassifier("Default","../data/test", "", false);
+  ASSERT_TRUE((orgBox.area() - (orgBox & r).area()) < 500);
 }
